85a6d37bf575cf3a4db11b144f8c3a303f5f0c82
Dhanush S H <dhanushsh700@gmail.com>
2026-08-18 17:25:13 +0530

    Add Basic Method

---
+++ Java for OOP/2_BasicMethod.java
@@ -0,0 +1,9 @@
+class BasicMethod{
+    static int add(int a,int b){
+        return a + b;
+    }
+    public static void main(String[] args){
+        int result = add(15,35);
+        System.out.println("Result: "+result);
+    }
+}
